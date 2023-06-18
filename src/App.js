2e65980377507512c6d62795123d6744f3638d99
kushtarbek <dr.kushtarbek@gmail.com>
2023-06-17 23:46:32 -0500

    render components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from "./data.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function renderJsonData(data) {
+  return data.map( content => {
+    return (
+      <div>
+        <h4>{content.type}</h4>
+        {renderChildren(content.children)}
+      </div>
+    )
+  })
+}
+
+function renderChildren( children ) {
+  return children.map(child => {
+    switch (child.type) {
+      case 'container':
+        return (
+          <div style={child.style.webStyle}>
+            {renderChildren(child.children)}
+          </div>
+        );
+        case 'text':
+          return (
+            <p style={child.style.webStyle}>{child.data.value}</p>
+            );
+        case 'image':
+          return (
+            <img src={child.value} style={child.style.webStyle} alt="" />
+          );
+        case 'textfield':
+          return (
+            <input
+            type='text'
+            placeholder={child.data.placeholder}
+            value={child.data.value}
+            style={child.style.webStyle}
+            />
+          );
+        case 'textarea':
+          return (
+            <textarea
+            placeholder={child.data.placeholder}
+            value={child.data.value}
+            style={child.style.webStyle}
+            />
+          );
+        case 'button':
+          return (
+            <button style={child.style.webStyle}>{child.data.title}</button>
+          );
+         case 'dropdown':
+          return (
+            <div>
+              {children && children.map( item => {
+                return (
+                  <form>
+                    {item.title }
+                    <select name="quality" id="id">
+                      <option value="" selected="selected">{children.item}</option>
+                    </select>
+                  </form>
+                
+                )
+            })}
+            </div>
+          );
+        default: 
+        return null;
+    }
+  })
+};
+
+const App = () => {   
+  return ( 
+    <div className='App'>
+      {renderJsonData(Data.views)}
     </div>
   );
 }
---
+++ src/builder-webcomponents.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Data from "./data.json";
+
+
+function JsonDataRenderer({ data }) {
+  const renderJsonData = (jsonData) => {
+    return jsonData.map(content => {
+      return (
+        <div key={content.type}>
+          <h4>{content.type}</h4>
+          {renderChildren(content.children)}
+        </div>
+      );
+    });
+  }
+
+  const renderChildren = (children) => {
+    return children.map(child => {
+      switch (child.type) {
+        case 'container':
+          return (
+            <div key={child.type} style={child.style.webStyle}>
+              {renderChildren(child.children)}
+            </div>
+          );
+        case 'text':
+          return (
+            <p key={child.type} style={child.style.webStyle}>{child.data.value}</p>
+          );
+        case 'image':
+          return (
+            <img key={child.type} src={child.value} style={child.style.webStyle} alt="" />
+          );
+        case 'textfield':
+          return (
+            <input
+              key={child.type}
+              type="text"
+              placeholder={child.data.placeholder}
+              value={child.data.value}
+              style={child.style.webStyle}
+            />
+          );
+        case 'textarea':
+          return (
+            <textarea
+              key={child.type}
+              placeholder={child.data.placeholder}
+              value={child.data.value}
+              style={child.style.webStyle}
+            />
+          );
+        case 'button':
+          return (
+            <button key={child.type} style={child.style.webStyle}>{child.data.title}</button>
+          );
+        default:
+          return null;
+      }
+    });
+  }
+
+  return (
+    <div>
+      {renderJsonData(data.views)}
+    </div>
+  );
+}
+
+// Usage
+function BuilderWebComponents() {
+  const jsonData = {
+    "views": [
+      // Your JSON data
+      
+    ]
+  };
+
+  return (
+    <JsonDataRenderer data={jsonData} />
+  );
+}
